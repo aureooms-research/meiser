42430a405425796e79e497d9023ef72ff116ccb7
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-22 15:47:45 +0200

    fix definition of X+Y hyperplanes

--- learning/xy.py
+++ learning/xy.py
@@ -6,11 +6,11 @@ def tuples(n):
         n.
 
         >>> list(tuples(4))
-        [(-1, 1, -1, 1), (-1, 1, 1, -1), (1, -1, -1, 1), (1, -1, 1, -1)]
+        [(1, 1, -1, -1)]
 
         >>> from math import factorial
         >>> binom = lambda n , k : factorial(n)/factorial(k)/factorial(n-k)
-        >>> len(list(tuples(8))) == (2*binom(4,2))**2
+        >>> len(list(tuples(16))) == binom(16//2,2)**2
         True
 
         >>> isinstance(next(tuples(4)), tuple)
@@ -22,7 +22,7 @@ def tuples(n):
 
     for left in pairs(n//2) :
 
-        for right in pairs(n//2) :
+        for right in negpairs(n//2) :
 
             yield left + right
 
@@ -30,8 +30,13 @@ def pairs(n):
 
     for pair in ksum.tuples(2,n) :
 
-        i = pair.index(1)
-        yield pair[:i] + (-1,) + pair[i+1:]
+        yield pair
 
+def negpairs(n):
+
+    for pair in pairs(n):
+
+        i = pair.index(1)
         j = pair.index(1, i+1)
-        yield pair[:j] + (-1,) + pair[j+1:]
+
+        yield pair[:i] + (-1,) + pair[i+1:j] + (-1,) + pair[j+1:]
