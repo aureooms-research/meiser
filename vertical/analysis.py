01751cc6662d8bbc5bfd5e2221d39cf94a83ebe3
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-22 11:57:01 +0200

    backup analysis draft

--- vertical/analysis.py
+++ vertical/analysis.py
@@ -1,4 +1,36 @@
-#!/usr/bin/env python3
 
-from argparse import ArgumentsParser
-from sympy import *
+from argparse import ArgumentParser
+from itertools import count
+from math import log, floor
+
+parser = ArgumentParser('How many comparisons dude')
+parser.add_argument('k', type=int, help='k.')
+
+args = parser.parse_args()
+
+k = args.k
+
+def locate(n,k):
+
+    s = k
+
+    O = 10
+    r = min(O * n * log(n,2),n**k)
+
+    while n > 0:
+
+        p = max(1, floor(n / s))
+
+        # number of comparisons
+        yield p * s * r / n
+
+        n -= p
+        s = min(2 * s - 2, n)
+        r -= 2
+
+
+
+for n in count(2):
+
+    c = sum(locate(n,k))
+    print(k,n,c,c/n**2/log(n,2)**2)
